1c1dfc8b70f188d6d6fdaf819a67f06cead3a8c0
Ace <pikachu.kinkoid@gmail.com>
2021-01-29 19:09:51 +0200

    add 3d scene

--- Assets/Scripts/IsometricPlayerMovementController.cs
+++ Assets/Scripts/IsometricPlayerMovementController.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IsometricPlayerMovementController : MonoBehaviour
 {
 
-    public float movementSpeed = 1f;
+    public float MovementSpeed = 5f;
+
+    public float ChargeSpeed;
     IsometricCharacterRenderer isoRenderer;
 
     Rigidbody2D rbody;
@@ -16,22 +19,46 @@ public class IsometricPlayerMovementController : MonoBehaviour
         isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
     }
 
-
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector2 currentPos = rbody.position;
-        
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        
+
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
         inputVector = Vector2.ClampMagnitude(inputVector, 1);
-        
-        Vector2 movement = inputVector * movementSpeed;
-        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-        
+
+        Vector2 movement = inputVector * MovementSpeed;
+        Vector2 newPos = currentPos + movement * Time.deltaTime;
+
+        // RaycastHit2D hit = Physics2D.Raycast(transform.position, inputVector, ovementSpeed * Time.deltaTime);
+        //
+        // if (hit.collider != null)
+        // {
+        //
+        //     // Debug.Log("Colider hit");
+        //     // Debug.Log(hit.collider.name);
+        //     // var vec = inputVector * (hit.distance - 0.05f);
+        //     // newPos = transform.position + new Vector3(vec.x, vec.y);
+        // }
+
         isoRenderer.SetDirection(movement);
         rbody.MovePosition(newPos);
+        // transform.position = newPos;
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+        }
+    }
+
+    public void Charge()
+    {
+        
     }
 }
