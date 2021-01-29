bf11dc4465271c7f1cbb5fbce264d3784d5128b7
Ace <pikachu.kinkoid@gmail.com>
2021-01-29 20:42:36 +0200

    shamefur dispray

---
+++ Assets/Scripts/3D/CharacterController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+namespace _3D
+{
+    public class CharacterController : MonoBehaviour
+    {
+        public Camera cam;
+
+        public Rigidbody rb => GetComponent<Rigidbody>();
+
+        public float MovementSpeed = 20;
+
+        public Vector3 ProcessInputVector(Vector2 input) 
+        { 
+            Quaternion cameraRotation = cam.transform.rotation;
+            var phase = (cameraRotation.eulerAngles.y * Mathf.Deg2Rad);
+//        var deltaSin = Mathf.Sin(phase + Mathf.Asin(input.x));
+//        var deltaCos = Mathf.Cos(phase + Mathf.Asin(input.y));
+
+            var inputRads = Mathf.Atan2(-input.y, input.x);
+            var modifiedInputRads = inputRads - phase;
+            var deltaSin = Mathf.Sin(modifiedInputRads);
+            var deltaCos = Mathf.Cos(modifiedInputRads);
+
+            var deltaX = deltaSin 
+                         * input.magnitude 
+                ;
+            var deltaZ = deltaCos
+                         * input.magnitude
+                ;
+            //* -1; // Invert Z
+
+            var newDeltaVector = new Vector3(deltaX, 0, deltaZ);
+            return newDeltaVector;
+        }
+
+        public void FixedUpdate()
+        {
+            Move();
+        }
+
+        public void Move()
+        {
+            Vector2 axes = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            var offset = ProcessInputVector(-axes);
+            var tp = transform.position;
+            var newPos = tp + (offset * (MovementSpeed * Time.fixedTime));
+            rb.MovePosition(newPos);
+            var direction = newPos - tp;
+            // transform.LookAt(direction);
+        }
+    }
+}
