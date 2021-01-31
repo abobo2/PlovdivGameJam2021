8e8f248333b4a3d7a5a9c14c90ea1730d56719bd
Ace <pikachu.kinkoid@gmail.com>
2021-01-31 14:30:51 +0200

    change camera distance

--- Assets/Scripts/BasicCameraFollow.cs
+++ Assets/Scripts/BasicCameraFollow.cs
@@ -6,13 +6,15 @@ public class BasicCameraFollow : MonoBehaviour
 	public Transform followTarget;
 	
 	public float moveSpeed;
+
+	public float FollowDistance = 150;
 	
 	void Update () 
 	{
 		if(followTarget != null)
 		{
 			Ray r = new Ray( followTarget.transform.position, -transform.forward);
-			var targetPos = r.GetPoint(100);
+			var targetPos = r.GetPoint(FollowDistance);
 			Vector3 velocity = (targetPos - transform.position) * moveSpeed;
 			transform.position = Vector3.SmoothDamp (transform.position, targetPos, ref velocity, 1.0f, Time.deltaTime);
 		}
