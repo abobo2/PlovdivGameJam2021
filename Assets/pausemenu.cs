ce3c870d7fa4d498ab952fa0e3205cab60fdcc6f
IvanNakov10 <Vani311220066@gmail.com>
2021-01-31 12:34:37 +0200

    pausemennuv,1

---
+++ Assets/pausemenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour
+{
+    public static bool GameISpaused = false;
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameISpaused)
+            {
+                Resume();
+            }    else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume ()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameISpaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameISpaused = true;
+    }
+}
